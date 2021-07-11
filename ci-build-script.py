f6cbb87bdc9f2ba4cfceaa2cc35fdfa612e617ba
Per Macha <macha@univention.de>
2021-07-12 00:00:00 +0200

    fix: replace deprecated CI_BUILD_ID
    and also replace CI_PIPELINE_TAG

--- ci-build-script.py
+++ ci-build-script.py
@@ -15,7 +15,6 @@ import sh  # pylint: disable=import-error
 sh2 = sh(_out='/dev/stdout', _err='/dev/stderr')  # pylint: disable=not-callable
 
 MANDATORY_ENV_VARS = (
-    'CI_PIPELINE_TAG',
     'DOCKER_COMPOSE_BUILD_FILES',
     'UPX_IMAGE_REGISTRY',
 )
@@ -65,19 +64,19 @@ def main():
         sha_hash = sh.git('rev-parse', 'HEAD').stdout.rstrip().decode('ascii')
         os.environ['CI_COMMIT_SHA'] = sha_hash
 
-    if 'CI_BUILD_ID' not in os.environ:
-        os.environ['CI_BUILD_ID'] = '4711'
+    if 'CI_PIPELINE_ID' not in os.environ:
+        os.environ['CI_PIPELINE_ID'] = 'none'
 
     docker_compose_build_files = os.environ['DOCKER_COMPOSE_BUILD_FILES']
     upx_image_registry = os.environ['UPX_IMAGE_REGISTRY']
-    ci_pipeline_tag = os.environ['CI_PIPELINE_TAG']
+    ci_pipeline_id = os.environ['CI_PIPELINE_ID']
 
     print('DEBUG')
     print(os.environ)
 
     sh2.cp(
         '.env.univention-directory-manager-rest.example',
-        '.env.univention-directory-manager-rest'
+        '.env.univention-directory-manager-rest',
     )
     sh2.docker_compose(
         docker_compose_build_files.split(),
@@ -85,8 +84,8 @@ def main():
         '--parallel',
     )
 
-    image_name = '{}container-udm-rest/udm:{}test'.format(
-        upx_image_registry, ci_pipeline_tag
+    image_name = '{}container-udm-rest/udm:{}-test'.format(
+        upx_image_registry, ci_pipeline_id
     )
     try:
         add_version_label(image_name)
