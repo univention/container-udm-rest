78cd15b05dd7a352dc7453c4126e5c8de31c104d
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2023-07-02 19:17:37 +0200

    feat: Make "process-join-data.py" executable

--- docker/udm-rest-api-join-data-loader/command.sh
+++ docker/udm-rest-api-join-data-loader/command.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-# TODO: Make script executable, remove "python3"
 # TODO: Apply all files in /join-data in a sorted way
-python3 /usr/local/bin/process-join-data.py /join-data/portal.yaml
+process-join-data.py /join-data/univention-portal.yaml.j2
