58f16f7ed081901e1443d69dcaa936c5927649f8
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2023-07-24 23:25:05 +0200

    feat(join-data-loader): Ensure user Administrator exists

--- docker/udm-rest-api-join-data-loader/command.sh
+++ docker/udm-rest-api-join-data-loader/command.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 # TODO: Apply all files in /join-data in a sorted way
+process-join-data.py /join-data/00-umc-init.yaml.j2
+process-join-data.py /join-data/10-univention-ldap-server.yaml.j2
 process-join-data.py /join-data/univention-portal.yaml.j2
