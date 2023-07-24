d901f6a7922152e8286a89c01400c179164aa91a
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2023-07-24 23:49:17 +0200

    feat(join-data-loader): Ensure user saml-idp-user is created

--- docker/udm-rest-api-join-data-loader/command.sh
+++ docker/udm-rest-api-join-data-loader/command.sh
@@ -4,4 +4,5 @@
 process-join-data.py /join-data/00-umc-init.yaml.j2
 process-join-data.py /join-data/10-univention-ldap-server.yaml.j2
 process-join-data.py /join-data/univention-portal.yaml.j2
+process-join-data.py /join-data/misc-saml.yaml.j2
 process-join-data.py /join-data/35-univention-management-console-module-udm.yaml.j2
