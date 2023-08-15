83c3b75d7999c9308f0799ad2e77ce37c4f252eb
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2023-08-15 22:13:40 +0200

    feat!: Removing join-data-loader
    BREAKING CHANGE: The functionality has been moved into the repository
    "stack-data".

--- docker/udm-rest-api-join-data-loader/command.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# TODO: Apply all files in /join-data in a sorted way
-process-join-data.py /join-data/00-umc-init.yaml.j2
-process-join-data.py /join-data/10-univention-ldap-server.yaml.j2
-process-join-data.py /join-data/univention-portal.yaml.j2
-process-join-data.py /join-data/misc-saml.yaml.j2
-process-join-data.py /join-data/35-univention-management-console-module-udm.yaml.j2
