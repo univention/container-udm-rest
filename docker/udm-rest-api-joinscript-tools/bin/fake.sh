cb3bf2842ebc61e5a10dc82942a824080caa6dd8
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2023-08-15 22:17:23 +0200

    feat!: Remove container joinscript-tools
    The tooling has been moved into the repository "stack-data".

--- docker/udm-rest-api-joinscript-tools/bin/fake.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-echo "Faking command: \"$0 $*\"" 1>&2
--- docker/udm-rest-api-joinscript-tools/dev-fixup-path.sh
+++
@@ -1,8 +0,0 @@
-# shellcheck shell=bash
-
-# This file has to be sourced to update the path
-#
-# Indented usage is for testing out the tooling locally. With the adjusted PATH
-# the scripts will run directly out of the sources if mounted correctly into the
-# container.
-export PATH=/src/docker/udm-rest-api-joinscript-tools/bin:$PATH
