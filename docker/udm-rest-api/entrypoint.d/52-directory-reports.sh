2cb90ad527e05e440b7e5d2dfa63ccb74b45a62c
Andreas Niemann <a.niemann@olsitec.de>
2024-01-24 15:15:38 +0100

    feat(helm)!: BSI-compliant deployment
    BREAKING CHANGES:
    
    This projects Helm chart is no longer compatible with existing configurations.

--- docker/udm-rest-api/entrypoint.d/52-directory-reports.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# SPDX-License-Identifier: AGPL-3.0-only
-# SPDX-FileCopyrightText: 2023 Univention GmbH
-
-set -euxo pipefail
-
-############################################################
-# Configure Univention Directory Reports
-ucr commit /etc/univention/directory/reports/config.ini
