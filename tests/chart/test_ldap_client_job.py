3912fdde3019c5d411c934aef75bcaf39efda940
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2025-06-06 12:53:05 +0200

    test: Split ldap related test configuration into two separate modules
    The chart is special in the sense that it does have multiple workload resources:
    
    - Deployment
    - Job
    - CronJob
    
    The idea is to run the client related tests per workload which uses the client.
    This has the drawback of double checking some aspects of the way how the secret
    is generated. The benefit is that the tests describe per workload the expected
    behavior and can this way better serve as documentation of the intended
    behavior.
    
    Issue: univention/dev/internal/team-nubus#892

---
+++ tests/chart/test_ldap_client_job.py
@@ -0,0 +1,28 @@
+# SPDX-License-Identifier: AGPL-3.0-only
+# SPDX-FileCopyrightText: 2025 Univention GmbH
+
+"""
+Verify `Job` specific LDAP client configuration.
+
+The `Job` to update the `univentionObjectIdentifier` is receiving all
+information via environment variables, including the password.
+"""
+
+from univention.testing.helm.client.ldap import (
+    LdapAuth,
+    LdapAuthUsageViaEnv,
+    LdapConnectionUri,
+)
+
+
+class TestLdapAuth(LdapAuthUsageViaEnv, LdapAuth):
+    config_map_name = "release-name-udm-rest-api"
+    secret_name = "release-name-udm-rest-api-ldap"
+    workload_resource_kind = "Job"
+
+    path_ldap_bind_dn = "data.LDAP_ADMIN_USER"
+
+
+class TestLdapConnectionJob(LdapConnectionUri):
+
+    workload_resource_kind = "Job"
