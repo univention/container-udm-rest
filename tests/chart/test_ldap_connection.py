f0487969ff667d82a794797f97d9313a39e1be26
Dimitri Papoutsis <dimitri.papoutsis.extern@univention.de>
2025-10-08 10:44:37 +0200

    refactor: conform LDAP authentication to latest helm chart tests
    univention/dev/internal/team-nubus#1441

--- tests/chart/test_ldap_connection.py
+++ tests/chart/test_ldap_connection.py
@@ -1,34 +1,13 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 # SPDX-FileCopyrightText: 2025 Univention GmbH
 
-"""
-Verify the LDAP client configuration for the UDM Rest API.
-
-The UDM Rest API is currently using most of the information out of the UCR. The
-related tests are decorated with `pytest.skip`.
-
-The container also as the file `/etc/ldap/ldap.conf` to configure the regular
-ldap clients.
-"""
-
 import re
 
-from univention.testing.helm.client.ldap import (
-    AuthPassword,
-    ConnectionUri,
-    ConnectionUriViaConfigMap,
-    SecretViaVolume,
-)
+from univention.testing.helm.client.ldap import (ConnectionUri,
+                                                 ConnectionUriViaConfigMap)
 
 
-class TestAuth(SecretViaVolume, AuthPassword):
-    config_map_name = "release-name-udm-rest-api"
-    secret_name = "release-name-udm-rest-api-ldap"
-
-    path_ldap_bind_dn = "data.LDAP_ADMIN_USER"
-
-
-class TestLdapConnectionLdapConf(ConnectionUriViaConfigMap, ConnectionUri):
+class TestUdmRestApiUsesLdapConnectionLdap(ConnectionUriViaConfigMap, ConnectionUri):
     """
     Verify the regular ldap client configuration in `/etc/ldap/ldap.conf`
     """
@@ -45,3 +24,7 @@ class TestLdapConnectionLdapConf(ConnectionUriViaConfigMap, ConnectionUri):
         ldap_conf = config_map.findone(self.path_ldap_conf)
         re_match = re.search(r'^URI\s+(?P<ldap_uri>.*?)$', ldap_conf, flags=re.MULTILINE)
         return re_match.group('ldap_uri')
+
+
+class TestJobUpdateUniventionObjectIdentifierUsesLdapConnection(ConnectionUri):
+    workload_kind = "Job"
